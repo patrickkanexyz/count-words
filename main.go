82662104b24c9e9d25500ba25b2c6bc75432d4b0
Patrick Kane <patrick@patrickkane.xyz>
2024-04-12 16:27:02 -0700

    implemented basic -l functionality

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	// Define commandline flags
 	var cFlag = flag.Bool("c", false, "Print out number of bytes in file.")
+    var lFlag = flag.Bool("l", false, "Print out number of lines in file.")
 	flag.Parse()
 
 	filenames := flag.Args()
@@ -17,6 +18,12 @@ func main() {
     if *cFlag {
         for _, filename := range filenames {
             count_bytes(filename)
+        }
+    }
+
+    if *lFlag {
+        for _, filename := range filenames {
+            count_lines(filename)
         }
     }
 
@@ -28,4 +35,21 @@ func count_bytes(filename string) {
         log.Fatal(err)
     }
     fmt.Printf("%d %s\n", len(data), filename)
+}
+
+func count_lines(filename string) {
+    data, err := os.ReadFile(filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    count := 0;
+
+    for _, runeValue := range data {
+        if runeValue == '\n' {
+            count++
+        }
+    }
+
+    fmt.Printf("%d %s\n", count, filename)
 }
