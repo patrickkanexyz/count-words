f54f57c5c48a8ab9c8c5c66edc4c85cc0a7c196f
Patrick Kane <patrick@patrickkane.xyz>
2024-04-14 18:23:47 -0700

    refactored whole program into simple state machine

--- main.go
+++ main.go
@@ -10,93 +10,61 @@ import (
 
 func main() {
 	// Define commandline flags
-	var cFlag = flag.Bool("c", false, "Print out number of bytes in file.")
-    var lFlag = flag.Bool("l", false, "Print out number of lines in file.")
-    var wFlag = flag.Bool("w", false, "Print out number of words in file.")
+	//var cFlag = flag.Bool("c", false, "Print out number of bytes in file.")
+    //var lFlag = flag.Bool("l", false, "Print out number of lines in file.")
+    //var wFlag = flag.Bool("w", false, "Print out number of words in file.")
 	flag.Parse()
 
 	filenames := flag.Args()
 
-    if *cFlag {
-        for _, filename := range filenames {
-            count_bytes(filename)
+    for _, filename := range filenames {
+        data, err := os.ReadFile(filename)
+        if err != nil {
+            log.Fatal(err)
         }
-    }
 
-    if *lFlag {
-        for _, filename := range filenames {
-            count_lines(filename)
-        }
-    }
 
-    if *wFlag {
-        for _, filename := range filenames {
-            count_words(filename)
-        }
-    }
+        // The data variable is a byte stream. While it mostly behaves like a
+        // regular string, it is ASCII formated and not UTF-8/unicode formated.
+        // This is a problem because character/rune testing functions from
+        // the standard library work on UTF-8/unicode runes and not ASCII.
+        //
+        // The reason for the data[:] is because we can type cast slices, but
+        // not byte streams.
+        unicode_data := string(data[:]) 
 
-}
-
-func count_bytes(filename string) {
-    data, err := os.ReadFile(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf("%d %s\n", len(data), filename)
-}
-
-func count_lines(filename string) {
-    data, err := os.ReadFile(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    count := 0;
-
-    for _, runeValue := range data {
-        if runeValue == '\n' {
-            count++
-        }
-    }
-
-    fmt.Printf("%d %s\n", count, filename)
-}
-
-func count_words(filename string) {
-    data, err := os.ReadFile(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    const (
-        IN = 0
-        OUT = 1
+        // Most of the word count utility can be modeled as a state machine.
+        // Here we define the states.
+        // OUT: we are outside of a word.
+        // IN: we are inside a word.
+        const (
+            OUT = 0
+            IN = 1
         )
 
-    is_word := OUT
-    count := 0
+        // Define default state.
+        word_state := OUT
 
-    myString := string(data[:])
+        byte_count := len(data)
+        line_count := 0
+        word_count := 0
 
-    for _, runeValue := range myString {
-        if unicode.IsSpace(runeValue) {
-            is_word = OUT
-        }
-        if ! unicode.IsSpace(runeValue) {
-            if is_word == OUT {
-                is_word = IN
-                count++
+        for _, runeValue := range unicode_data {
+            if unicode.IsSpace(runeValue) {
+                word_state = OUT
+
+                if runeValue == '\n' {
+                    line_count++
+                }
+            }
+            if ! unicode.IsSpace(runeValue) {
+                if word_state == OUT {
+                    word_count++
+                }
+                word_state = IN
             }
         }
+
+        fmt.Printf("%d %d %d %s\n", byte_count, word_count, line_count, filename)
     }
-
-    fmt.Printf("%d %s\n", count, filename)
-
-}
-
-func is_whitespace(c byte) bool {
-    if c == '\n' || c == '\t' || c == ' ' {
-        return true
-    }
-    return false
 }
